a3d35d09743ba4cd9ba5034c22866b780f07e6ca
Timothy Dayley <tdayley@gmail.com>
2015-07-20 11:57:49 -0700

    Re-design, add update info

--- src/main.c
+++ src/main.c
@@ -8,14 +8,19 @@ static Window *s_main_window;
 static TextLayer *s_time_layer;
 static TextLayer *s_day_layer;
 static TextLayer *s_date_layer;
-static TextLayer *s_gap_layer;
+static TextLayer *s_gap_layer_one;
+static TextLayer *s_gap_layer_two;
+static TextLayer *s_gap_layer_three;
+static TextLayer *s_gap_layer_four;
 static TextLayer *s_weather_layer;
+static TextLayer *s_info_layer;
 
 static GFont s_providence_font;
 static GFont s_visitor_font;
 
 char day_text[32];
 char date_text[32];
+char info_text[32];
 
 
 static void update_time() {
@@ -38,8 +43,8 @@ static void update_time() {
   // Display this time on the TextLayer
   text_layer_set_text(s_time_layer, buffer);
   
-  strftime(day_text, sizeof(day_text), " %A", tick_time);
-  strftime(date_text, sizeof(date_text), "%B %e ", tick_time);
+  strftime(day_text, sizeof(day_text), "      %A", tick_time);
+  strftime(date_text, sizeof(date_text), "%B %e     ", tick_time);
   text_layer_set_text(s_day_layer, day_text);
   text_layer_set_text(s_date_layer, date_text);
 }
@@ -48,7 +53,7 @@ static void tick_handler(struct tm *tick_frame, TimeUnits units_changed) {
   update_time();
   
   // Get weather update every 30 minutes
-  if(tick_frame->tm_min % 1 == 0) {
+  if(tick_frame->tm_min % 5 == 0) {
     // Begin dictionary
     DictionaryIterator *iter;
     app_message_outbox_begin(&iter);
@@ -58,6 +63,10 @@ static void tick_handler(struct tm *tick_frame, TimeUnits units_changed) {
 
     // Send the message!
     app_message_outbox_send();
+    
+    // Add update info text to TextLayer
+    strftime(info_text, sizeof(info_text), "Updated:\n%I:%M", tick_frame);
+    text_layer_set_text(s_info_layer, info_text);
   }
 }
 
@@ -107,57 +116,80 @@ static void outbox_sent_callback(DictionaryIterator *iterator, void *context) {
 }
 
 static void main_window_load(Window *window) {
-  Layer *window_layer = window_get_root_layer(window);
-  GRect window_bounds = layer_get_bounds(window_layer);
+  //Layer *window_layer = window_get_root_layer(window);
+  //GRect window_bounds = layer_get_bounds(window_layer);
   
   s_visitor_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_VISITOR_16));
   s_providence_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_PROVIDENCE_48));
   
+  // Create day TextLayer
+  s_day_layer = text_layer_create(GRect(0, 0, 144, 22));
+  text_layer_set_background_color(s_day_layer, GColorBlack);
+  text_layer_set_text_color(s_day_layer, GColorWhite);
+  text_layer_set_text_alignment(s_day_layer, GTextAlignmentLeft);
+  text_layer_set_font(s_day_layer, fonts_get_system_font(FONT_KEY_GOTHIC_18));
+  
+  // Create right gap TextLayer
+  s_gap_layer_one = text_layer_create(GRect(0, 22, 18, 2));
+  text_layer_set_background_color(s_gap_layer_one, GColorBlack);
+  // Create left gap TextLayer
+  s_gap_layer_two = text_layer_create(GRect(130, 22, 30, 2));
+  text_layer_set_background_color(s_gap_layer_two, GColorBlack);
+  
   // Create time TextLayer
-  s_time_layer = text_layer_create(GRect(0, 0, 144, 60));
+  s_time_layer = text_layer_create(GRect(0, 24, 144, 60));
   text_layer_set_background_color(s_time_layer, GColorBlack);
   text_layer_set_text_color(s_time_layer, GColorWhite);
   text_layer_set_text_alignment(s_time_layer, GTextAlignmentCenter);
   text_layer_set_font(s_time_layer, s_providence_font);
   
-  // Create gap TextLayer
-  s_gap_layer = text_layer_create(GRect(0, 60, 144, 3));
-  text_layer_set_background_color(s_gap_layer, GColorWhite);
-  text_layer_set_text_alignment(s_gap_layer, GTextAlignmentCenter);
-  
-  // Create day TextLayer
-  s_day_layer = text_layer_create(GRect(0, 63, 144, 20));
-  text_layer_set_background_color(s_day_layer, GColorBlack);
-  text_layer_set_text_color(s_day_layer, GColorWhite);
-  text_layer_set_text_alignment(s_day_layer, GTextAlignmentLeft);
-  text_layer_set_font(s_day_layer, s_visitor_font);
+  // Create right gap TextLayer
+  s_gap_layer_three = text_layer_create(GRect(0, 84, 18, 2));
+  text_layer_set_background_color(s_gap_layer_three, GColorBlack);
+  // Create left gap TextLayer
+  s_gap_layer_four = text_layer_create(GRect(130, 84, 30, 2));
+  text_layer_set_background_color(s_gap_layer_four, GColorBlack);
   
   // Create date TextLayer
-  s_date_layer = text_layer_create(GRect(0, 80, 144, 20));
+  s_date_layer = text_layer_create(GRect(0, 86, 144, 24));
   text_layer_set_background_color(s_date_layer, GColorBlack);
   text_layer_set_text_color(s_date_layer, GColorWhite);
   text_layer_set_text_alignment(s_date_layer, GTextAlignmentRight);
-  text_layer_set_font(s_date_layer, s_visitor_font);
+  text_layer_set_font(s_date_layer, fonts_get_system_font(FONT_KEY_GOTHIC_18));
   
   // Create weather TextLayer
-  s_weather_layer = text_layer_create(GRect(0, 100, 144, 60));
+  s_weather_layer = text_layer_create(GRect(5, 110, 75, 200));
   text_layer_set_background_color(s_weather_layer, GColorWhite);
   text_layer_set_text_color(s_weather_layer, GColorBlack);
-  text_layer_set_text_alignment(s_weather_layer, GTextAlignmentCenter);
+  text_layer_set_text_alignment(s_weather_layer, GTextAlignmentLeft);
   text_layer_set_font(s_weather_layer, s_providence_font);
   
+  // Create info TextLayer
+  s_info_layer = text_layer_create(GRect(75, 125, 70, 200));
+  text_layer_set_background_color(s_info_layer, GColorWhite);
+  text_layer_set_text_color(s_info_layer, GColorBlack);
+  text_layer_set_text_alignment(s_info_layer, GTextAlignmentCenter);
+  text_layer_set_font(s_info_layer, fonts_get_system_font(FONT_KEY_GOTHIC_14));
+  
   // Add it as a child layer to the Window's root layer
-  layer_add_child(window_get_root_layer(window), text_layer_get_layer(s_time_layer));
-  layer_add_child(window_get_root_layer(window), text_layer_get_layer(s_gap_layer));
   layer_add_child(window_get_root_layer(window), text_layer_get_layer(s_day_layer));
+  layer_add_child(window_get_root_layer(window), text_layer_get_layer(s_gap_layer_one));
+  layer_add_child(window_get_root_layer(window), text_layer_get_layer(s_gap_layer_two));
+  layer_add_child(window_get_root_layer(window), text_layer_get_layer(s_time_layer));
+  layer_add_child(window_get_root_layer(window), text_layer_get_layer(s_gap_layer_three));
+  layer_add_child(window_get_root_layer(window), text_layer_get_layer(s_gap_layer_four));
   layer_add_child(window_get_root_layer(window), text_layer_get_layer(s_date_layer));
   layer_add_child(window_get_root_layer(window), text_layer_get_layer(s_weather_layer));
+  layer_add_child(window_get_root_layer(window), text_layer_get_layer(s_info_layer));
 }
 
 static void main_window_unload(Window *window) {
   // Destroy TextLayer
   text_layer_destroy(s_time_layer);
-  text_layer_destroy(s_gap_layer);
+  text_layer_destroy(s_gap_layer_one);
+  text_layer_destroy(s_gap_layer_two);
+  text_layer_destroy(s_gap_layer_three);
+  text_layer_destroy(s_gap_layer_four);
   text_layer_destroy(s_day_layer);
   text_layer_destroy(s_date_layer);
   text_layer_destroy(s_weather_layer);
